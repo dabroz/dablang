5692d59fcfcd44860b239a0c6dc96ead46625ae2
Tomasz Dabrowski <dabroz@gmail.com>
2011-11-25 22:57:57 +0100

    CLI stub.

---
+++ sources/cli/stdafx.h
@@ -0,0 +1,25 @@
+// Dab CLI
+
+#include <typeinfo>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <assert.h>
+#include <algorithm>
+
+using namespace std;
+
+namespace llvm
+{
+	class Module;
+	class Value;
+	class Function;
+	class Type;
+	class FunctionType;
+};
+
+#include "dab_header.h"
